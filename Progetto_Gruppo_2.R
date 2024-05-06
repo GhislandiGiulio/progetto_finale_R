ef060502392994af389fef7d1a236defba9e715a
GGhislandi <giulio.ghislandi@itsrizzoli.it>
2024-05-06 14:18:31 +0200

    prima versione

---
+++ Progetto_Gruppo_2.R
@@ -0,0 +1,36 @@
+library(readxl)
+library(dplyr)
+
+setwd("C:/Users/FrancescoIuorio/Downloads")
+DatiX <- read_excel("GRUPPO_1-2._France_Bakery.xlsx")
+
+head(DatiX)
+names(DatiX)
+str(DatiX)
+summary(DatiX)
+
+head(is.na(DatiX), 7)
+
+apply(DatiX, 2, function(dataset) sum(is.na(dataset)))
+
+unique(DatiX$social)
+unique(DatiX$lang_value)
+
+DatiInutili <- sum(is.na(DatiX$text) & is.na(DatiX$score_rating))
+head(DatiInutili)
+
+sum(DatiX$lang_value == 'fr')
+
+DatiBaguette <- filter(DatiX, lang_value == 'fr')
+sum(is.na(DatiBaguette$text))
+sum(is.na(DatiBaguette$score_rating))
+
+cat('Togliamo due colonne perché inutili e una perché vuota(riscrivere)')
+
+DatiFR <- select(DatiBaguette, -social, -likes, -lang_value)
+
+head(DatiFR)
+
+unique(DatiFR$Players)
+
+DatiFR$Players <- as.factor(DatiFR$Players)
