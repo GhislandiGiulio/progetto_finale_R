13509b190146189bd72908e53ef3ef5d36e9c01a
NobruLcnITS <bruno.luciano@itsrizzoli.it>
2024-05-11 19:29:45 +0200

    Aggiunta cross validation

--- Progetto_Gruppo_2.R
+++ Progetto_Gruppo_2.R
@@ -383,23 +383,23 @@ system.time(for(i in 1:k){
   validation_set <-
     matrice_training_set2 [folds$subsets[folds$which == i], ]
   set.seed(123)
-  RandomForest <- randomForest(
+  support_vector_machine <- svm(
     y= dfm_training_set[folds$subsets[folds$which != i], ]
     @docvars$sentiment ,
     x=matrice_training_set, do.trace=FALSE, ntree=96)
-  df_RF_prediction <- predict(RandomForest, 
+  df_SV_prediction <- predict(support_vector_machine, 
                               newdata= validation_set, 
                               type="class")
-  class_table <- table("Predictions"= df_RF_prediction,
+  class_table <- table("Predictions"= df_SV_prediction,
                        "Actual"=dfm_training_set[folds$subsets[folds$which == i], ]@docvars$sentiment)
   print(class_table)
   df<-confusionMatrix( class_table, mode = "everything")
-  df_measures_RF<-paste0("conf.mat.rf",i)
-  assign(df_measures_RF,df)
+  df_measures_SV<-paste0("conf.mat.rf",i)
+  assign(df_measures_SV,df)
 })
 
 #Dataset vuoto
-df_RF_prediction <- data.frame(col1=vector(), col2=vector(), col3=vector(), col4 = vector())
+df_SV_prediction <- data.frame(col1=vector(), col2=vector(), col3=vector(), col4 = vector())
 
 #riempiamo il dataset con le performance metrics
 for(i in mget(ls(pattern = "conf.mat.rf")) ) { #conf.mat.rf
@@ -408,20 +408,97 @@ for(i in mget(ls(pattern = "conf.mat.rf")) ) { #conf.mat.rf
   F1_negative <- p$F1[1]
   F1_neutral <- p$F1[2]
   F1_positive <- p$F1[3]
-  df_RF_prediction <- rbind(df_RF_prediction , cbind(Accuracy , F1_negative ,
+  df_SV_prediction <- rbind(df_SV_prediction , cbind(Accuracy , F1_negative ,
                                                      F1_neutral, F1_positive ))
   
 }
 
 # sostituzione dei valori mancanti con 0
-df_RF_prediction [is.na(df_RF_prediction )] <- 0
+df_SV_prediction [is.na(df_SV_prediction )] <- 0
 
 # estrazione del valore medio di accuracy e f1
-AverageAccuracy_RF <- mean(df_RF_prediction[, 1] )
-AverageF1_RF<- mean(colMeans(df_RF_prediction[-1] ))
+AverageAccuracy_SV <- mean(df_SV_prediction[, 1] )
+AverageF1_SV<- mean(colMeans(df_SV_prediction[-1] ))
 
 # visualizzazione dei valori medi
-print(AverageAccuracy_RF)
-print(AverageF1_RF)
+print(AverageAccuracy_SV)
+print(AverageF1_SV)
 
 
+  ####  COMPARAZIONE RISULTATI
+
+#Comparazione
+library(reshape2)
+
+# ACCURACY 
+#Creo un dataframe per NB
+AccNB <- as.data.frame(AverageAccuracy_NB )
+#Rinomino la colonna
+colnames(AccNB)[1] <- "NB"
+
+#Creo un dataframe per RF
+AccRF <- as.data.frame(AverageAccuracy_RF )
+#Rinomino la colonna
+colnames(AccRF)[1] <- "RF"
+
+#Creo un dataframe per SV
+AccSV<- as.data.frame(AverageAccuracy_SV )
+#Rinomino la colonna
+colnames(AccSV)[1] <- "SV"
+
+#Unisco in un unico dataframe i valori di accuracy dei tre modelli
+Accuracy_models <- cbind(AccNB, AccRF, AccSV)
+Accuracy_models
+
+
+Accuracy_models_Melt <-melt(Accuracy_models)
+str(Accuracy_models_Melt)
+
+#Creo un grafico per i valori di accuracy 
+plot_accuracy <- ggplot(Accuracy_models_Melt, aes(x=variable, y=value, color = variable)) +
+  geom_boxplot() + xlab("Algorithm") + ylab(label="Values of accuracy") +
+  labs(title = "Cross-validation with k =5: values of accuracy") + coord_flip() +
+  theme_bw() +
+  guides(color=guide_legend(title="Algorithms")) +
+  theme(plot.title = element_text(color = "black", size = 12, face = "italic"),
+        axis.title.x =element_text(size=12,face="bold"),
+        axis.title.y =element_text(size=12, face = "plain"),
+        axis.text= element_text(size =10, face = "italic")
+  )
+
+
+# F1 SCORE 
+# Replico gli stessi step per f1 score
+
+#NB
+F1NB <- as.data.frame(AverageF1_NB)
+colnames(F1NB)[1] <- "NB"
+#RF
+F1RF<- as.data.frame(AverageF1_RF )
+colnames(F1RF)[1] <- "RF"
+#SV
+F1SV <- as.data.frame(AverageF1_SV)
+colnames(F1SV)[1] <- "SV"
+#DATAFRAME
+f1_models <- cbind(F1NB, F1RF, F1SV)
+f1_models
+
+f1_models_melt <-melt(f1_models)
+str(f1_models_melt)
+
+#Creo il grafico
+plot_f1 <- ggplot(f1_models_melt, aes(x=variable, y=value, color = variable)) +
+  geom_boxplot() + xlab("Algorithm") + ylab(label="Values of f1") +
+  labs(title = "Cross-validation with k =5: values of f1") + coord_flip() +
+  theme_bw() +
+  guides(color=guide_legend(title="Algorithms")) +
+  theme(plot.title = element_text(color = "black", size = 12, face = "italic"),
+        axis.title.x =element_text(size=12,face="bold"),
+        axis.title.y =element_text(size=12, face = "plain"),
+        axis.text= element_text(size =10, face = "italic")
+  )
+
+
+library(gridExtra)
+#Visualizzo i due grafici 
+grid.arrange(plot_accuracy, plot_f1, nrow=2)
